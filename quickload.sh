36cb10d54bae768c032a464aec80d2111a7f3082
Joel B <smol@toast.cyou>
2023-04-30 15:26:49 -0400

    now extract relevent data

--- quickload.sh
+++ quickload.sh
@@ -1,4 +1,4 @@
 # build with buildkit enabled
 DOCKER_BUILDKIT=1 docker compose -f docker-compose.dev.yml up -d --build
 # run with buildkit enabled
-DOCKER_BUILDKIT=1 docker compose up -d -v weights:/usr/src/app/weights -v datadb:/data/db/ -p 8008:8008 --build
+docker run -d -v weights:/usr/src/app/weights -v datadb:/data/db/ -p 8008:8008 {IMAGE_ID}
